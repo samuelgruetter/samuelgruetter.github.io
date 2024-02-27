40156ba03ce14ebff35b84daed06ff7dbf19156c
Samuel Gruetter <saemi472@gmail.com>
2024-02-27 14:48:43 -0500

    add link to omnisemantics artifact

--- _publications/run_bibtex2html.sh
+++ _publications/run_bibtex2html.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-OPTIONS="-d -r -revkeys -nodoc -nofooter -nokeywords -noabstract -linebreak -nf url pdf"
+OPTIONS="-d -r -revkeys -nodoc -nofooter -nokeywords -noabstract -linebreak -nf url pdf -nf artifact artifact"
 
 bibtex2html $OPTIONS -o ../publications publications.bib
 bibtex2html $OPTIONS -o ../reports reports.bib
