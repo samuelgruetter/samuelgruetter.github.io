e52561a8d342f423b3d98237e01b51d19fd23b8b
Samuel Gruetter <saemi472@gmail.com>
2024-03-02 13:08:19 -0500

    add code linkes to all publications
    and remove bib entries because they now contain a non-standard "code"
    field and I don't test/use them enough

--- _publications/run_bibtex2html.sh
+++ _publications/run_bibtex2html.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-OPTIONS="-d -r -revkeys -nodoc -nofooter -nokeywords -noabstract -linebreak -nf url pdf -nf artifact artifact"
+# in all lines that start with "code", unescape all underscores
+# (zotero's better bibtex escapes them because the "code" field is not an url field)
+UNESCAPE_IN_CODE_FIELD='/^ *code/s/\\_/_/g'
+
+sed -i -e "$UNESCAPE_IN_CODE_FIELD" publications.bib
+sed -i -e "$UNESCAPE_IN_CODE_FIELD" reports.bib
+
+OPTIONS="-d -r -revkeys -nodoc -nofooter -nokeywords -noabstract -nobiblinks -nobibsource -linebreak -nf code code -nf url pdf"
 
 bibtex2html $OPTIONS -o ../publications publications.bib
 bibtex2html $OPTIONS -o ../reports reports.bib
