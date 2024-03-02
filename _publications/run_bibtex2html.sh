5f529410257a36a5a64c443f8393acbf26fbb87b
Samuel Gruetter <saemi472@gmail.com>
2024-03-02 13:23:30 -0500

    PDF in all caps

--- _publications/run_bibtex2html.sh
+++ _publications/run_bibtex2html.sh
@@ -7,7 +7,7 @@ UNESCAPE_IN_CODE_FIELD='/^ *code/s/\\_/_/g'
 sed -i -e "$UNESCAPE_IN_CODE_FIELD" publications.bib
 sed -i -e "$UNESCAPE_IN_CODE_FIELD" reports.bib
 
-OPTIONS="-d -r -revkeys -nodoc -nofooter -nokeywords -noabstract -nobiblinks -nobibsource -linebreak -nf code code -nf url pdf"
+OPTIONS="-d -r -revkeys -nodoc -nofooter -nokeywords -noabstract -nobiblinks -nobibsource -linebreak -nf code code -nf url PDF"
 
 bibtex2html $OPTIONS -o ../publications publications.bib
 bibtex2html $OPTIONS -o ../reports reports.bib
